95c2af363417f8b3223a52a12be05f90b4d1bc9f
Darwin Yang(楊文智) <darwinyang@gamania.com>
2020-01-20 09:36:51 +0800

    Initial commit

---
+++ python/scripts/show_success.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+print(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd2in7
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+import qrcode
+import qrcode.image.pil
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+try:
+
+    epd = epd2in7.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear(0xFF)
+
+    font24 = ImageFont.truetype(os.path.join(
+        picdir, 'RobotoSlab-Regular.ttf'), 24)
+    font18 = ImageFont.truetype(os.path.join(
+        picdir, 'RobotoSlab-Regular.ttf'), 18)
+    
+    # Drawing on the Horizontal image
+    logging.info("1.Drawing on the Horizontal image...")
+    Himage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(Himage)
+    draw.text((10, 50), 'Payment success! ^-^', font = font24, fill = 0)
+    
+    epd.display(epd.getbuffer(Himage))
+    time.sleep(10)
+    epd.Clear(0xFF)
+
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd2in7.epdconfig.module_exit()
+    exit()
---
+++ server.js
@@ -0,0 +1,55 @@
+var paymentModule = require('iota-payment')
+const app = require('express')();
+let { PythonShell } = require('python-shell')
+var path = require('path');
+var ip = require("ip");
+
+app.get("/", function (req, res) {
+    res.send('hello world from iota-payment-raspberry-example');
+});
+
+app.get("/create_payment", function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/index.html'));
+
+});
+
+var options = {
+    mount: '/payments',
+    value: 1
+}
+
+let server = paymentModule.createServer(app, options)
+
+// Start server with iota-payment module on '/payments'
+server.listen(3000, function () {
+    console.log(`Server started on ${ip.address()}:3000 `)
+})
+
+
+//Create an event handler which is called, when a payment was successfull
+var onPaymentCreated = function (payment) {
+    console.log("onPaymentCreated", payment)
+
+    PythonShell.run('./python/scripts/show_qr_code.py', { args: [payment.address, payment.amount] }, function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        console.log('results: %j', results);
+    });
+}
+
+//Create an event handler which is called, when a payment was successfull
+var onPaymentSuccess = function (payment) {
+    console.log("onPaymentSuccess", payment)
+    PythonShell.run('./python/scripts/show_success.py', {}, function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        console.log('results: %j', results);
+    });
+}
+
+
+//Assign the event handler to an event:
+paymentModule.on('paymentCreated', onPaymentCreated);
+paymentModule.on('paymentSuccess', onPaymentSuccess);
+
